5867a16d53cd8aba8bf11d1ccb966b70f0e6ca8c
Clayton Wheeler <cswh@umich.edu>
2012-05-21 15:38:53 -0400

    Open file and parse the header in the constructor.

--- spec/bio/maf/parser_spec.rb
+++ spec/bio/maf/parser_spec.rb
@@ -5,14 +5,15 @@ module Bio
 
     describe Parser do
 
-      describe "#open_file" do
-        it "opens for reading the file passed as an argument"
+      describe "creation" do
+        it "opens a file specified as a String argument"
+        it "takes an IO object as an open file"
         it "raises an error when the file does not exist"
         it "raises an error when the file is not in MAF format"
         it "parses the MAF header"
       end
 
-      describe "#header" do
+     describe "#header" do
         it "raises an error when a MAF file is not open"
         it "provides version information"
         it "provides the scoring scheme"
