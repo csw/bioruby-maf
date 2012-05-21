daf869b13cf04405e2b4bc936a45c552fc152c48
Clayton Wheeler <cswh@umich.edu>
2012-05-21 19:00:36 -0400

    Specified and implemented header parsing.

--- lib/bio/maf/parser.rb
+++ lib/bio/maf/parser.rb
@@ -1,8 +1,101 @@
 module Bio
   module MAF
 
+    class ParseError < Exception; end
+
+    class Header
+      attr_accessor :vars, :alignment_params
+
+      def initialize(vars, params)
+        @vars = vars
+        @alignment_params = params
+      end
+
+      def version
+        vars[:version]
+      end
+
+      def scoring
+        vars[:scoring]
+      end
+
+    end
+
+    class Block
+      attr_reader :score, :sequences
+    end
+
+    class Sequence
+      attr_accessor 
+    end
+
     class Parser
 
+      attr_accessor :header, :file_spec, :f, :r
+
+      def initialize(file_spec)
+        @file_spec = file_spec
+        @f = File.open(file_spec)
+        @r = LineReader.new(@f)
+        _parse_header
+      end
+
+      def _parse_header
+        l1 = r.next_line
+        raise ParseError, "Not a MAF file: #{file_spec}" unless l1 =~ /^##maf\s/
+        vars = {}
+        parts = l1.strip.split
+        parts[1..parts.size].each do |var_spec|
+          if var_spec =~ /(\w+)=(.*)/
+            vars[$1.to_sym] = $2
+          else
+            raise ParseError, "Malformed MAF variable: #{var_spec}"
+          end
+        end
+        raise ParseError, "Missing MAF version!" unless vars.has_key? :version
+        params = nil
+        while true
+          line = r.next_line.strip
+          case line
+          when /^#\s*(\S.*)/
+            if params
+              params << ' '
+            else
+              params = ''
+            end
+            params << $1
+          when /^#/, /^$/
+            break
+          end
+          if f =~ /#\s*.*/
+            r.rewind
+            break
+          end
+        end
+        @header = Header.new(vars, params)
+      end
+
+    end
+
+    class LineReader
+      attr_reader :f
+
+      def initialize(f)
+        @f = f
+      end
+
+      def next_line
+        if @again
+          @again = false
+          return @last
+        else
+          return @last = @f.readline
+        end
+      end
+
+      def rewind
+        @again = true
+      end
     end
     
   end
--- spec/bio/maf/parser_spec.rb
+++ spec/bio/maf/parser_spec.rb
@@ -3,24 +3,105 @@ require 'spec_helper'
 module Bio
   module MAF
 
+    describe Header do
+      before(:each) do
+        @p = Parser.new(TestData + 't1.maf')
+      end
+
+      it "provides version information" do
+        @p.header.version.should == '1'
+      end
+      it "provides the scoring scheme" do
+        @p.header.scoring.should == 'humor.v4'
+      end
+      it "provides alignment parameters" do
+        @p.header.alignment_params.should =~ /humor.v4 R=30/
+      end
+
+      it "presents multiline parameters correctly" do
+        @p.header.alignment_params.should == "humor.v4 R=30 M=10 /cluster/data/hg15/bed/blastz.mm3/axtNet300/chr1.maf /cluster/data/hg15/bed/blastz.rn3/axtNet300/chr1.maf"
+      end
+
+      it "provides arbitrary parameters"
+    end
+
     describe Parser do
 
       describe "creation" do
         it "opens a file specified as a String argument"
         it "takes an IO object as an open file"
-        it "raises an error when the file does not exist"
-        it "raises an error when the file is not in MAF format"
-        it "parses the MAF header"
+        it "raises an error when the filfe does not exist" do
+          expect {
+            Parser.new("/doesnotexist")
+          }.to raise_error(Errno::ENOENT)
+        end
+        it "raises an error when the file is not in MAF format" do
+          expect {
+            Parser.new(TestData + '../../Rakefile')
+          }.to raise_error
+        end
       end
 
-     describe "#header" do
-        it "raises an error when a MAF file is not open"
-        it "provides version information"
-        it "provides the scoring scheme"
-        it "provides alignment parameters"
-        it "provides arbitrary parameters"
+      describe "#header" do
+        it "parses the MAF header" do
+          p = Parser.new(TestData + 't1.maf')
+          p.header.should_not be_nil
+        end
       end
 
+      context "at end of file" do
+        describe "#parse_block" do
+          it "returns nil"
+        end
+      end
+
+      describe "#parse_block" do
+        it "returns an alignment block" do
+          pending
+          p = Parser.new(TestData + 't1.maf')
+          b = p.parse_block()
+          b.should_not be_nil
+        end
+        it "raises an exception for malformed data"
+      end
+
+    end
+
+    describe LineReader do
+      describe "creation" do
+        it "takes a file-like object as argument" do
+          expect {
+            @f = File.open(TestData + 't1.maf', 'r')
+            @reader = LineReader.new(@f)
+            @f.close
+          }.not_to raise_error
+        end
+      end
+
+      context "with a file open" do
+        before(:each) do
+          @f = File.open(TestData + 't1.maf', 'r')
+          @reader = LineReader.new(@f)
+        end
+
+        after(:each) do
+          @f.close
+        end
+
+        describe "#next_line" do
+          it "returns the next line of the file" do
+            @reader.next_line.should =~ /^##maf/
+          end
+        end
+
+        describe "#rewind" do
+          it "causes #next_line to return the same line again" do
+            l1 = @reader.next_line
+            @reader.rewind
+            @reader.next_line.should == l1
+          end
+        end
+      end
     end
     
   end
