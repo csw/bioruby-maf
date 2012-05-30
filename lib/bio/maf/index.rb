fce9364197c65ae7bba924525de9d6b0f118725e
Clayton Wheeler <cswh@umich.edu>
2012-05-30 13:45:19 -0400

    Build index tuples. Closes #14.

--- lib/bio/maf/index.rb
+++ lib/bio/maf/index.rb
@@ -1,10 +1,14 @@
 require 'dbi'
 
+require 'bio-ucsc-api'
+
 module Bio
 
   module MAF
 
     class SQLiteIndex
+
+      attr_accessor :sequence, :db
 
       def self.build(parser, idx_path)
         if File.exist? idx_path
@@ -20,7 +24,12 @@ module Bio
         @db = DBI.connect("DBI:SQLite3:#{path.to_s}", "", "")
       end
 
-      
+      def index_tuple(block)
+        seq = block.sequences.find { |s| s.source == sequence }
+        seq_end = seq.start + seq.size
+        bin = Bio::Ucsc::UcscBin.bin_from_range(seq.start, seq_end)
+        return [bin, seq.start, seq_end]
+      end
     end
     
   end
--- spec/bio/maf/index_spec.rb
+++ spec/bio/maf/index_spec.rb
@@ -16,20 +16,31 @@ module Bio
       describe "with mm8_chr7 data" do
         before(:each) do 
           @p = Parser.new(TestData + 'mm8_chr7_tiny.maf')
-          @idx = SQLiteIndex.new
+          @idx = SQLiteIndex.new(":memory:")
           @idx.sequence = "mm8.chr7"
         end
         after(:each) do
-          @idx.db.close
+          @idx.db.disconnect
         end
-      end
-      describe "#index_tuple" do
-        it "preserves the start and end" do
-          block = @p.parse_block
-          seq = block.sequences.find { |s| s.source == @idx.sequence }
-          tuple = @idx.index_tuple(block)
-          tuple[1].should == seq.start
-          tuple[2].should == seq.start + seq.size
+
+        describe "#index_tuple" do
+          it "preserves the start and end" do
+            block = @p.parse_block
+            seq = block.sequences.find { |s| s.source == @idx.sequence }
+            tuple = @idx.index_tuple(block)
+            tuple[1].should == seq.start
+            tuple[2].should == seq.start + seq.size
+          end
+        end
+
+        describe "#index_tuple" do
+          it "sets the bin correctly" do
+            block = @p.parse_block
+            seq = block.sequences.find { |s| s.source == @idx.sequence }
+            tuple = @idx.index_tuple(block)
+            # TODO: independently check this
+            tuple[0].should == 1195
+          end
         end
       end
 
