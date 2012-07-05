b5cb7eeee2bcc93b4f4f8450415b1aa6dc01d8d6
Clayton Wheeler <cswh@umich.edu>
2012-07-05 13:05:34 -0400

    Reorganized step definitions.

--- features/step_definitions/gap-filling_steps.rb
+++ features/step_definitions/gap-filling_steps.rb
@@ -2,22 +2,22 @@ Given /^chromosome reference sequence:$/ do |string|
   @refseq = Bio::FastaFormat.new(string)
 end
 
-When /^map species (\S+) as (\S+)$/ do |sp1, sp2|
-  @tiler.species_map[sp1] = sp2
-end
-
 When /^tile ([^:\s]+):(\d+)-(\d+)( with the chromosome reference)?$/ do |seq, i_start, i_end, ref_p|
   @tiler = Bio::MAF::Tiler.new
   @tiler.index = @idx
   @tiler.parser = @parser
   @tiler.reference = @refseq.seq if ref_p
   @tiler.interval = Bio::GenomicInterval.zero_based(seq,
-                                               i_start.to_i,
-                                               i_end.to_i)
+                                                    i_start.to_i,
+                                                    i_end.to_i)
 end
 
 When /^tile with species \[(.+?)\]$/ do |species_text|
   @tiler.species = species_text.split(/,\s*/)
+end
+
+When /^map species (\S+) as (\S+)$/ do |sp1, sp2|
+  @tiler.species_map[sp1] = sp2
 end
 
 When /^write the tiled data as FASTA$/ do
