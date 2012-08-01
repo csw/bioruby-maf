cc5b408ad0810612507e14b27187ca65eecded48
Clayton Wheeler <cswh@umich.edu>
2012-08-01 04:13:38 -0400

    Use IO#write instead of IO#puts.

--- lib/bio/maf/writer.rb
+++ lib/bio/maf/writer.rb
@@ -20,8 +20,8 @@ module Bio::MAF
     end
 
     def write_header(header)
-      f.puts "##maf #{flatten_vars(header.vars)}"
-      f.puts "##{header.alignment_params}" if header.alignment_params
+      f.write "##maf #{flatten_vars(header.vars)}\n"
+      f.write "##{header.alignment_params}\n" if header.alignment_params
     end
 
     def write_blocks(blocks)
